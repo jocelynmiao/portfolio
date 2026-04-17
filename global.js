1d2adb4cade7eb99cf713b07ec6fd3d476bd8d30
jocelynmiao <jocelynmiao@hotmail.com>
2026-04-16 21:41:41 -0700

    js file

---
+++ global.js
@@ -0,0 +1,81 @@
+console.log("IT'S ALIVE!");
+
+function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'CV/', title: 'CV' },
+  { url: 'https://github.com/jocelynmiao', title: 'Github' }
+];
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                 
+  : "/portfolio/"; 
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  let a = document.createElement('a');
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  if (url.startsWith('http')) {
+    a.target = "_blank";
+    }
+
+  a.href = url;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+    }
+
+  a.textContent = title;
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+const select = document.querySelector('.color-scheme select');
+
+if ("colorScheme" in localStorage) {
+  const saved = localStorage.colorScheme;
+  document.documentElement.style.setProperty('color-scheme', saved);
+  select.value = saved; 
+}
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+const form = document.querySelector('form');
+
+if (form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = new FormData(form);
+    let params = [];
+    for (let [name, value] of data) {
+        params.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+    }
+    const url = form.action + "?" + params.join("&");
+    console.log(url);
+    location.href = url;
+  });
+}
