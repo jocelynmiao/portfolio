c55f5887df79b5be8fb4a134bb6f5a350146febd
jocelynmiao <jocelynmiao@hotmail.com>
2026-04-24 00:48:44 -0700

    project js

--- global.js
+++ global.js
@@ -1,5 +1,3 @@
-console.log("IT'S ALIVE!");
-
 function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
@@ -78,4 +76,39 @@ if (form) {
     console.log(url);
     location.href = url;
   });
+}
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+
+  project.forEach(p => {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${p.title}</${headingLevel}>
+      <img src="${p.image}" alt="${p.title}">
+      <p>${p.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  });
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
